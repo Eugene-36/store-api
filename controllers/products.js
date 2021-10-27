3f12e7f6d2bbabc2197794c73f62a3fd9ad8a0a1
Eugene-36 <67147219+Eugene-36@users.noreply.github.com>
2021-10-27 17:05:43 +0300

    rest-api-app

---
+++ controllers/products.js
@@ -0,0 +1,18 @@
+const Product = require('../models/product');
+
+const getAllProductsStatic = async (req, res) => {
+  const products = await Product.find({
+    name: 'vase table',
+  });
+  res.status(200).json({ products, nbHints: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHints: products.length });
+};
+
+module.exports = {
+  getAllProducts,
+  getAllProductsStatic,
+};
